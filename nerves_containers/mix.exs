bfa612e2eea26493ee3af03ec2434caf0899115c
Steffen Deusch <steffen@deusch.me>
2021-09-09 22:55:45 +0200

    update dependencies, use nerves_motd

--- nerves_containers/mix.exs
+++ nerves_containers/mix.exs
@@ -62,7 +62,7 @@ defmodule NervesContainers.MixProject do
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
-      {:nerves_pack, "~> 0.4.0", targets: @all_targets},
+      {:nerves_pack, "~> 0.5.0", targets: @all_targets},
 
       # Dependencies for specific targets
       {:nerves_containers_rpi,
